f02b319fe2e6a30036176fa9cd76248619fca19a
Sjoerd <m@desjoerd.com>
2016-05-24 10:04:48 +0200

    Add first form

--- src/DeSjoerd.FeedbackApp/Controllers/FeedbackController.cs
+++ src/DeSjoerd.FeedbackApp/Controllers/FeedbackController.cs
@@ -1,3 +1,4 @@
+using DeSjoerd.FeedbackApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeSjoerd.FeedbackApp.Controllers
@@ -5,7 +6,14 @@ namespace DeSjoerd.FeedbackApp.Controllers
     public class FeedbackController : Controller
     {
         public IActionResult Index() {
-            return View();
+            return View(new Feedback());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([FromForm] Feedback feedback)
+        {
+            return View("Index");
         }
     }
 }
