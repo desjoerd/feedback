27c405ca185639d640c1ab4f582d52fd0e860338
Sjoerd <m@desjoerd.com>
2016-06-06 08:41:25 +0200

    Move connectionstring to appsettings.json

--- src/DeSjoerd.FeedbackApp/Startup.cs
+++ src/DeSjoerd.FeedbackApp/Startup.cs
@@ -3,20 +3,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DeSjoerd.FeedbackApp
 {
     public class Startup
-    {   
-        public Startup()
-        {
+    {
+        private readonly IConfiguration _configuration;
 
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            _configuration = builder.Build();
         }
 
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc();
-
-            services.AddDbContext<Models.FeedbackContext>(options => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=DeSjoerd.FeedbackApp;Trusted_Connection=True;Integrated Security=true"));
+            
+            services.AddDbContext<Models.FeedbackContext>(options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
         }
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
